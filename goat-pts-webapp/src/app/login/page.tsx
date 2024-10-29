12986bced0025748f8a078f3cd6aaed295a2cce6
Generalist <hello@generalist.dev>
2024-10-28 21:38:23 -0400

    downgrade next 15 to 14

--- goat-pts-webapp/src/app/login/page.tsx
+++ goat-pts-webapp/src/app/login/page.tsx
@@ -1,9 +1,11 @@
-import { LoginForm } from "@/components/login-form"
+'use client';
+
+import { LoginForm } from '@/components/login-form';
 
 export default function Page() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center px-4">
-      <LoginForm />
-    </div>
-  )
+    return (
+        <div className="flex h-screen w-full items-center justify-center px-4">
+            <LoginForm />
+        </div>
+    );
 }
